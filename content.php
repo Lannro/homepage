fdb3cf5857a754d6837ccb213c97969247ab3397
Tyler Cinkant <tyler.cinkant@ubc.ca>
2019-04-30 12:04:30 -0700

    Updating main text

--- content.php
+++ content.php
@@ -3,23 +3,21 @@ $path = preg_split("@/@", $_SERVER['REQUEST_URI'], NULL, PREG_SPLIT_NO_EMPTY);
 echo "
 <div class='content centered table'>
 	<div class='row'>
-		<div class='sidebar cell'>
-			<img src='/files/portrait.jpg' width=200 height=260>
-		</div>
-		<div class='maincontent cell'>
+		<div class='content cell'>
+			<img class='portrait' src='/files/portrait.jpg' width=200 height=260>
 			Click here for <a href='./test'>test.</a>
 			<br>
 			<br>
-			Tyler Cinkant is a professional programmer, game developer, and systems analyst.
+			Tyler Cinkant is a professional Programmer, Game Developer, and Systems Analyst.
 			<br>
 			<br>
-			I graduated from University of British Columbia in 2011 with a Bachelor of Science in Computer Science.
+			He graduated from University of British Columbia in 2011 with a Bachelor of Science in Computer Science.
 			<br>
 			<br>
-			During my last year at UBC, I was one of three founding members of an indie game studio, Gigatross Games. I worked as the lead developer for several major projects.
+			During his last year at UBC, he was one of three founding members of an indie game studio, Gigatross Games. He worked as the lead developer for several major projects.
 			<br>
 			<br>
-			After Gigatross Games, I began work at the University of British Columbia, in the UBC Learning Technology Hub, where I am currently employed as a Systems Analyst.
+			After Gigatross Games, he began work at the University of British Columbia, in the UBC Learning Technology Hub, where he is currently employed as a Systems Analyst.
 		</div>
 	</div>
 </div>
