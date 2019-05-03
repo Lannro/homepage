99bb7823948f6bc3c99c9f8bb37bdc71dda90f35
Tyler Cinkant <tyler.cinkant@ubc.ca>
2019-05-03 15:56:09 -0700

    Removing project content from systems page

--- systems/content.php
+++ systems/content.php
@@ -1,56 +1,5 @@
 <?php echo "
 <div class='content centered'>
-	<div class ='project'>
-		<div class='project-center'>
-			<a href='/projects/fantasybump'>
-				<img class='projecticon' src='/files/fb_icon.png' width=50 height=50>
-		</div>
-		<div class ='projecttitle project-center'>
-			Fantasy Bump
-			</a>
-			<div class ='projectdescription'>
-				1-6 player party bumping game.
-			</div>
-		</div>
-	</div>
-	<div class ='project'>
-		<div class='project-center'>
-			<a href='/projects/sinecannon'>
-				<img class='projecticon' src='/files/sc_icon.png' width=50 height=50>
-		</div>
-		<div class ='projecttitle project-center'>
-			Sine Cannon
-			</a>
-			<div class ='projectdescription'>
-				Algorithm-based deathmatch, made during a 24hr game jam.
-			</div>
-		</div>
-	</div>
-	<div class ='project'>
-		<div class='project-center'>
-			<a href='/projects/grandclassmelee2'>
-				<img class='projecticon' src='/files/gcm2_icon.jpg' width=50 height=50>
-		</div>
-		<div class ='projecttitle project-center'>
-			Grand Class Melee 2
-			</a>
-			<div class ='projectdescription'>
-				RPG Job-system arena brawler.
-			</div>
-		</div>
-	</div>
-	<div class ='project'>
-		<div class='project-center'>
-			<a href='/projects/grandclassmelee'>
-				<img class='projecticon' src='/files/gcm1_icon.jpg' width=50 height=50>
-		</div>
-		<div class ='projecttitle project-center'>
-			Grand Class Melee
-			</a>
-			<div class ='projectdescription'>
-				RPG Job-system arena brawler.
-			</div>
-		</div>
-	</div>
+	// TODO: create page
 </div>
 "?>
