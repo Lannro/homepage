4cddcb60759755dad0bbf4caa9bdf3f20b0967a2
Tyler Cinkant <tcinkant@gmail.com>
2019-05-31 11:40:48 -0700

    Adding download link to projects

--- projects/content.php
+++ projects/content.php
@@ -35,6 +35,7 @@ function display_project($con, $row){
 	$trailer =  $row['trailer'];
 	$banner_x =  $row['banner_x'];
 	$banner_y =  $row['banner_y'];
+	$download =  $row['download'];
 	
 	display_banner($banner_x, $banner_y, $tiny_name);
 	display_name($long_name);
@@ -43,11 +44,12 @@ function display_project($con, $row){
 	display_design($design);
 	display_trailer($trailer);
 	display_screenshots($tiny_name);
+	display_download($download);
 }
 
 
 function display_banner($banner_x, $banner_y, $tiny_name) {
-	echo '<div><img class="cell projectbanner" src="/files/'.$tiny_name.'_banner.png" width="'.$banner_x.'" height="'.$banner_y.'"></div>';
+	if($banner_x>0) echo '<div><img class="cell projectbanner" src="/files/'.$tiny_name.'_banner.png" width="'.$banner_x.'" height="'.$banner_y.'"></div>';
 }
 
 function display_name($long_name) {
@@ -83,6 +85,11 @@ function display_screenshots($tiny_name) {
 	}
 	echo'</div>	
 	</div>';
+}
+
+function display_download($download) {
+	if($download>0) echo '<div class="row"><h2>Download</h2>
+	<iframe frameborder="0" src="https://itch.io/embed/'.$download.'?border_width=2&amp;bg_color=3e4244&amp;fg_color=ffffff&amp;link_color=b0b0f5&amp;border_color=222222" width="554" height="169"></iframe><br><br></div>';
 }
 
 function display_project_list($con) {
