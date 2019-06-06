467d9974a6663866ded08903593a027022af8a70
Tyler Cinkant <tcinkant@gmail.com>
2019-06-06 11:56:33 -0700

    Adding pointer

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <?php
 	echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">	
 	<head>
-		<link rel="stylesheet" href="/files/styles.css?v1.2">
+		<link rel="stylesheet" href="/files/styles.css?v1.3">
 		<link rel="icon" href="/files/favicon.png" sizes="16x16" type="image/png">
 		<title>Tyler Cinkant</title>
 	</head>';
